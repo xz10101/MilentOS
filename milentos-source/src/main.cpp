469342c668fa5d8940b9544fddd88acb0e9ed29a
xz10101 <gureev.sash@ro.ru>
2026-06-12 15:45:13 +0300

    Добавленно минимальное ядро (без линкера и makefile'a), пока комплировать не стоит (добавлен бесконечный цикл что бы ОС не выключилась)

--- milentos-source/src/main.cpp
+++ milentos-source/src/main.cpp
@@ -1,9 +1,9 @@
 #include "main.h"
 
-void kernel_main() {
+extern "C" void kernel_main() {
     video[0] = "h";
     video[1] = "i";
     video[2] = "!";
     video[3] = "\0";
-    
+    while(true);
 }
